b0cda8c1dd138000fa9b89edc2fde02944170e91
Jasper van Gestel <jvangestel@gmail.com>
2018-07-24 21:15:44 +0200

    added labels to predictionmodel model

--- src/Prediction/Model/PredictionModelsModel.php
+++ src/Prediction/Model/PredictionModelsModel.php
@@ -9,5 +9,9 @@ class PredictionModelsModel extends \Gems_Model_JoinModel
     public function __construct()
     {
         parent::__construct('prediction-models', 'gems__prediction_models', 'gpm');
+        $this->set('gpm_source_id', 'label', $this->_('Source ID'));
+        $this->set('gpm_name', 'label', $this->_('Model name'));
+        $this->set('gpm_id_track', 'label', $this->_('Track'));
+        $this->set('gpm_url', 'label', $this->_('URL'));
     }
 }
