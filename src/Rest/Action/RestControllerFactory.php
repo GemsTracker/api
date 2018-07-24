1d798e75d7026ec25338c22055780cd1a72a99b4
Jasper van Gestel <jvangestel@gmail.com>
2018-07-24 21:19:06 +0200

    Updated RestController Factory

--- src/Rest/Action/RestControllerFactory.php
+++ src/Rest/Action/RestControllerFactory.php
@@ -3,6 +3,7 @@
 namespace Gems\Rest\Action;
 
 use Interop\Container\ContainerInterface;
+use Zalt\Loader\ProjectOverloader;
 use Zend\ServiceManager\Factory\FactoryInterface;
 use Zend\Expressive\Helper\UrlHelper;
 
@@ -11,9 +12,10 @@ class RestControllerFactory implements FactoryInterface
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
         $loader = $container->get('loader');
+        $db1 = $container->get('LegacyDb');
 
         $urlHelper = $container->get(UrlHelper::class);
 
-        return new $requestedName($container, $loader, $urlHelper);
+        return new $requestedName($loader, $urlHelper, $db1);
     }
 }
