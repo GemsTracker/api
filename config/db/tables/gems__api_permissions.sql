71b1c0f5e7912aa18285ed58cea8cb9faf819551
Jasper van Gestel <jvangestel@gmail.com>
2018-11-23 13:43:05 +0100

    more space for resources

--- config/db/tables/gems__api_permissions.sql
+++ config/db/tables/gems__api_permissions.sql
@@ -1,7 +1,7 @@
 CREATE TABLE gems__api_permissions (
   gapr_id           bigint(20) NOT NULL AUTO_INCREMENT,
   gapr_role         varchar(30) NOT NULL,
-  gapr_resource     varchar(30) NOT NULL,
+  gapr_resource     varchar(50) NOT NULL,
   gapr_permission   varchar(30) NOT NULL,
   gapr_allowed      tinyint(1) NOT NULL DEFAULT '0',
 
