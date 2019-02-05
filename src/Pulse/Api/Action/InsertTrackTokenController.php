dd68f268ff48b0503132192792a0d5d5d681118b
Jasper van Gestel <jvangestel@gmail.com>
2019-02-05 16:32:48 +0100

    Calculate next round order on insert survey

--- src/Pulse/Api/Action/InsertTrackTokenController.php
+++ src/Pulse/Api/Action/InsertTrackTokenController.php
@@ -9,6 +9,9 @@ use Gems\Rest\Exception\RestException;
 use Gems\Rest\Security\CheckContentTypeTrait;
 use Interop\Http\ServerMiddleware\DelegateInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Zend\Db\Adapter\Adapter;
+use Zend\Db\Sql\Expression;
+use Zend\Db\Sql\Sql;
 use Zend\Diactoros\Response\EmptyResponse;
 use Zend\Expressive\Helper\UrlHelper;
 
@@ -33,6 +36,11 @@ class InsertTrackTokenController extends RestControllerAbstract
     ];
 
     /**
+     * @var Adapter
+     */
+    protected $db;
+
+    /**
      * @var UrlHelper
      */
     protected $helper;
@@ -42,8 +50,9 @@ class InsertTrackTokenController extends RestControllerAbstract
      */
     protected $tracker;
 
-    public function __construct(\Gems_Tracker $tracker, UrlHelper $helper)
+    public function __construct(\Gems_Tracker $tracker, UrlHelper $helper, Adapter $db)
     {
+        $this->db = $db;
         $this->helper = $helper;
         $this->tracker = $tracker;
     }
@@ -75,6 +84,8 @@ class InsertTrackTokenController extends RestControllerAbstract
         }
 
         $tokenData['gto_id_round']          = '0';
+        $tokenData['gto_round_order']       = $this->getNextRoundOrder($tokenData);
+
 
         $respondentTrack = $this->tracker->getRespondentTrack($tokenData['gto_id_respondent_track']);
         $surveyId = $tokenData['gto_id_survey'];
@@ -103,5 +114,29 @@ class InsertTrackTokenController extends RestControllerAbstract
         }
 
         return new EmptyResponse(201);
+    }
+
+    protected function getNextRoundOrder($tokenData)
+    {
+        if (isset($tokenData['gto_round_description'], $tokenData['gto_id_respondent_track'])) {
+            $sql = new Sql($this->db);
+            $select = $sql->select();
+            $select->from('gems__rounds')
+                ->columns(['maxRoundOrder' => new Expression('MAX(gro_id_order)')])
+                ->join('gems__respondent2track', 'gr2t_id_track = gro_id_track', [])
+                ->where('gr2t_id_respondent_track', $tokenData['gto_id_respondent_track'])
+                ->where('gro_round_description', $tokenData['gto_round_description']);
+
+            $statement = $sql->prepareStatementForSqlObject($select);
+            $result = $statement->execute();
+
+            if ($result->valid() && $result->current()) {
+                $row = $result->current();
+                $maxValue = reset($row);
+
+                return $maxValue+1;
+            }
+        }
+        return 99999;
     }
 }
