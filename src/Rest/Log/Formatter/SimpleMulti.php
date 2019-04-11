bedd8a9ef68d5e83695a42819cce31a1c597c11c
Jasper van Gestel <jvangestel@gmail.com>
2019-04-11 08:39:17 +0200

    Added a logger function that doesn't ruin a multidimensional array

---
+++ src/Rest/Log/Formatter/SimpleMulti.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace Gems\Rest\Log\Formatter;
+
+
+use Zend\Log\Formatter\Simple;
+
+/**
+ * Fixes the way multidimensional arrays are written to JSON in the log
+ *
+ * Class SimpleMulti
+ * @package Gems\Rest\Log\Formatter
+ */
+class SimpleMulti extends Simple
+{
+    /**
+     * Formats data to be written by the writer.
+     *
+     * @param array $event event data
+     * @return array
+     */
+    public function format($event)
+    {
+        foreach ($event as $key => $value) {
+            $event[$key] = $this->normalize($value);
+        }
+
+        $output = $this->format;
+
+        foreach ($event as $name => $value) {
+            if ('extra' == $name && count($value)) {
+                $value = $this->normalize($value);
+            } elseif ('extra' == $name) {
+                // Don't print an empty array
+                $value = '';
+            }
+            $output = str_replace("%$name%", $value, $output);
+        }
+
+        if (isset($event['extra']) && empty($event['extra'])
+            && false !== strpos($this->format, '%extra%')
+        ) {
+            $output = rtrim($output, ' ');
+        }
+        return $output;
+    }
+}
