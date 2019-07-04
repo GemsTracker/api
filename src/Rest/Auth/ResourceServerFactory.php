4abf6f4987100b86dd53820bbb1f9497488c75fc
Jasper van Gestel <jvangestel@gmail.com>
2019-07-04 20:36:27 +0200

    Allow skipping of permissions of certs if disabled in config

--- src/Rest/Auth/ResourceServerFactory.php
+++ src/Rest/Auth/ResourceServerFactory.php
@@ -4,6 +4,7 @@
 namespace Gems\Rest\Auth;
 
 use Interop\Container\ContainerInterface;
+use League\OAuth2\Server\CryptKey;
 use League\OAuth2\Server\ResourceServer;
 use Zend\ServiceManager\Factory\FactoryInterface;
 
@@ -14,8 +15,14 @@ class ResourceServerFactory implements FactoryInterface
     {
         $config = $container->get('config');
 
+        $certificates        = $config['certificates'];
+        $passPhrase          = array_key_exists('passPhrase', $certificates) ? $certificates['passPhrase'] : null;
+        $keyPermissionsCheck = array_key_exists('keyPermissionsCheck', $certificates) ? $certificates['keyPermissionsCheck'] : true;
+
+        $pubKey = new CryptKey($certificates['public'], $passPhrase, $keyPermissionsCheck);
+
         return new ResourceServer(
-            $container->get('Gems\Rest\Auth\AccessTokenRepository'), $config['certificates']['public']
+            $container->get('Gems\Rest\Auth\AccessTokenRepository'), $pubKey
         );
     }
 }
