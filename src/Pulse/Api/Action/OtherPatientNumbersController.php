25e92b7feb2172a3e867d560f5d463e2286aad26
Jasper van Gestel <jvangestel@gmail.com>
2022-01-27 21:06:07 +0100

    Add support for dossier template previews from linked organizations

--- src/Pulse/Api/Action/OtherPatientNumbersController.php
+++ src/Pulse/Api/Action/OtherPatientNumbersController.php
@@ -12,6 +12,7 @@ use Laminas\Db\Sql\Predicate\PredicateSet;
 use Laminas\Db\Sql\Sql;
 use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ServerRequestInterface;
+use Pulse\Api\Repository\OtherPatientNumbersRepository;
 
 class OtherPatientNumbersController extends RestControllerAbstract
 {
@@ -30,36 +31,9 @@ class OtherPatientNumbersController extends RestControllerAbstract
         $patientNr = $request->getAttribute('patientNr');
         $organizationId = $request->getAttribute('organizationId');
 
-        $sql = new Sql($this->db);
-        $subSelect = $sql->select('gems__respondent2org')
-            ->columns(['gr2o_id_user'])
-            ->where([
-                'gr2o_patient_nr' => $patientNr,
-                'gr2o_id_organization' => $organizationId,
-            ]);
+        $otherPatientNumbersRepository = new OtherPatientNumbersRepository($this->db);
 
-        $currentOrganizationPredicate = new Predicate();
-        $currentOrganizationPredicate->equalTo('gr2o_id_organization', $organizationId);
-
-        $select = $sql->select('gems__respondent2org')
-            ->join('gems__organizations', 'gor_id_organization = gr2o_id_organization', [])
-            ->join('gems__reception_codes', 'gr2o_reception_code = grc_id_reception_code', [])
-            ->columns(['gr2o_id_organization', 'gr2o_patient_nr'])
-            ->where([
-                'grc_success' => 1,
-                'gr2o_id_user' => $subSelect,
-                new PredicateSet([
-                    $currentOrganizationPredicate,
-                    new Like('gor_accessible_by', '%'.(int)$organizationId.'%'),
-                ], PredicateSet::COMBINED_BY_OR),
-            ]);
-
-        $statement = $sql->prepareStatementForSqlObject($select);
-        $result = $statement->execute();
-
-        $fields = iterator_to_array($result);
-
-        $pairs = array_column($fields, 'gr2o_patient_nr', 'gr2o_id_organization');
+        $pairs = $otherPatientNumbersRepository->getAllPatientNumbers($patientNr, $organizationId);
 
         return new JsonResponse($pairs);
     }
