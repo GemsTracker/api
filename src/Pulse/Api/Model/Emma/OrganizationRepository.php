04c9f86d7366a3950edb7d843656ad2c911cb990
Jasper van Gestel <jvangestel@gmail.com>
2022-03-24 13:53:51 +0100

    Add match location to organization repository

--- src/Pulse/Api/Model/Emma/OrganizationRepository.php
+++ src/Pulse/Api/Model/Emma/OrganizationRepository.php
@@ -6,9 +6,15 @@ namespace Pulse\Api\Model\Emma;
 use Laminas\Db\Adapter\Adapter;
 use Laminas\Db\Sql\Expression;
 use Laminas\Db\Sql\Sql;
+use Laminas\Db\TableGateway\TableGateway;
 
 class OrganizationRepository
 {
+    /**
+     * @var int UserId
+     */
+    protected $currentUserId;
+
     /**
      * @var Adapter
      */
@@ -16,9 +22,43 @@ class OrganizationRepository
 
     protected $localOrganizations;
 
-    public function __construct(Adapter $db)
+    public function __construct(Adapter $db, $currentUserId)
     {
         $this->db = $db;
+        $this->currentUserId = $currentUserId;
+    }
+
+    /**
+     * @param string $name Location name
+     * @param int $organizationId Organization ID
+     * @return int|null Last insert value
+     */
+    public function createLocation($name, $organizationId)
+    {
+        if (strlen($name) > 250) {
+            $name = substr_replace($name, '...', 247);
+        }
+
+        $locationTable = new TableGateway('gems__locations', $this->db);
+        $result = $locationTable->insert([
+            'glo_name' => $name,
+            'glo_organizations' => ':'.$organizationId.':',
+            'glo_match_to' => $name,
+            'glo_changed' => new Expression('NOW()'),
+            'glo_changed_by' => $this->currentUserId,
+            'glo_created' => new Expression('NOW()'),
+            'glo_created_by' => $this->currentUserId,
+        ]);
+
+        if ($result) {
+            return (int)$locationTable->getLastInsertValue();
+        }
+        return null;
+    }
+
+    public function getCurrentUserId()
+    {
+        return $this->currentUserId;
     }
 
     protected function getLocalOrganizations()
@@ -83,5 +123,40 @@ class OrganizationRepository
         }
 
         return null;
+    }
+
+    public function matchLocation($name, $organizationId, $create = true)
+    {
+        $sql = new Sql($this->db);
+        $select = $sql->select();
+        $select->from('gems__locations')
+            ->columns(['glo_id_location']);
+
+        $select->where
+            ->nest()
+                ->equalTo('glo_match_to', $name)
+                ->or
+                ->like('glo_match_to', '%|'.$name.'|%')
+                ->or
+                ->like('glo_match_to', $name.'|%')
+                ->or
+                ->like('glo_match_to', '%|'.$name)
+            ->unnest()
+            ->and
+            ->equalTo('glo_active', 1)
+            ->and
+            ->equalTo('glo_organizations', ':'.$organizationId.':');
+
+        $statement = $sql->prepareStatementForSqlObject($select);
+        $result = $statement->execute();
+
+        if ($result->valid() && $result->current()) {
+            $match = $result->current();
+            return (int)$match['glo_id_location'];
+        }
+
+        if ($create) {
+            $this->createLocation($name, $organizationId);
+        }
     }
 }
