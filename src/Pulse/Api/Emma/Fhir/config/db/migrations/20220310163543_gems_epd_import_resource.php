ccf1ad4202c1a71a5083f8b0c48666bbf2da7820
Jasper van Gestel <jvangestel@gmail.com>
2022-03-24 16:04:45 +0100

    fix in migration

--- src/Pulse/Api/Emma/Fhir/config/db/migrations/20220310163543_gems_epd_import_resource.php
+++ src/Pulse/Api/Emma/Fhir/config/db/migrations/20220310163543_gems_epd_import_resource.php
@@ -41,7 +41,7 @@ class GemsEpdImportResource extends AbstractMigration
                 ->addColumn('geir_id_organization', 'biginteger', ['signed' => false, 'null' => true])
                 ->addColumn('geir_status', 'string', ['limit' => 32])
                 ->addColumn('geir_errors', 'text', ['null' => true])
-                ->addColumn('geir_duration', 'biginteger', ['signed' => false, 'null' => true])
+                ->addColumn('geir_duration', 'float', ['null' => true])
                 ->addColumn('geir_changed', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
                 ->addColumn('geir_changed_by', 'biginteger', ['signed' => true])
                 ->addColumn('geir_created', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
