df650ccbeda87c5a411a19dbc0ac328c07038ebd
Jasper van Gestel <jvangestel@gmail.com>
2022-04-06 14:03:47 +0200

    Appointment description from Emma now has both activity and reason in it. It should be stripped and added to the appointment info

--- src/Pulse/Api/Emma/Fhir/Model/Transformer/AppointmentActivityTransformer.php
+++ src/Pulse/Api/Emma/Fhir/Model/Transformer/AppointmentActivityTransformer.php
@@ -19,6 +19,31 @@ class AppointmentActivityTransformer extends \MUtil_Model_ModelTransformerAbstra
         $this->activityRepository = $activityRepository;
     }
 
+    protected function getActivityParts($activityDescription)
+    {
+        $pattern = '/ - (Links |Rechts |BDZ )?[\d]{1,3} min/m';
+        $result = preg_match($pattern, $activityDescription, $matches, PREG_OFFSET_CAPTURE);
+        if ($result !== 1) {
+            return ['activity' => $activityDescription];
+        }
+
+        if (count($matches)) {
+            $reason = substr($matches[0][0], 3);
+            $activityParts = [
+                'activity' => substr($activityDescription, 0, $matches[0][1]),
+                'reason' => $reason,
+            ];
+            if (count($matches) > 1) {
+                $activityParts['side'] = trim($matches[1][0]);
+                $activityParts['reason'] = substr($reason, (strlen($matches[1][0])));
+            }
+
+            return $activityParts;
+        }
+
+        return null;
+    }
+
     /**
      * From description to agenda activity ID
      *
@@ -37,9 +62,24 @@ class AppointmentActivityTransformer extends \MUtil_Model_ModelTransformerAbstra
             $organizatonId = $row['gap_id_organization'];
         }
 
-        $activityId = $this->activityRepository->matchActivity($row['description'], $organizatonId);
-        if ($activityId) {
-            $row['gap_id_activity'] = $activityId;
+        $activityParts = $this->getActivityParts($row['description']);
+        if ($activityParts !== null && isset($activityParts['activity'])) {
+            $activityId = $this->activityRepository->matchActivity($activityParts['activity'], $organizatonId);
+            if ($activityId) {
+                $row['gap_id_activity'] = $activityId;
+            }
+            if (isset($activityParts['reason'])) {
+                if (!isset($row['gap_info']) || !is_array($row['gap_info'])) {
+                    $row['gap_info'] = [];
+                }
+                $row['gap_info']['reason'] = $activityParts['reason'];
+            }
+            if (isset($activityParts['side'])) {
+                if (!isset($row['gap_info']) || !is_array($row['gap_info'])) {
+                    $row['gap_info'] = [];
+                }
+                $row['gap_info']['side'] = $activityParts['side'];
+            }
         }
 
         return $row;
--- test/Pulse/Api/Emma/Fhir/Model/Transformer/AppointmentActivityTransformerTest.php
+++ test/Pulse/Api/Emma/Fhir/Model/Transformer/AppointmentActivityTransformerTest.php
@@ -59,6 +59,71 @@ class AppointmentActivityTransformerTest extends TestCase
         $this->assertEquals($expected, $result);
     }
 
+    public function testAppointmentDescriptionWithReason()
+    {
+        $model = $this->getAppointmentModel();
+
+        $transformer = $this->getTransformer();
+        $data = [
+            'description' => 'consult - Links 12 min',
+            'gap_id_organization' => 1,
+        ];
+
+        $result = $transformer->transformRowBeforeSave($model, $data);
+
+        $expected = $data;
+        $expected['gap_id_activity'] = 32;
+        $expected['gap_info'] = [
+            'reason' => '12 min',
+            'side' => 'Links',
+        ];
+        $this->assertEquals($expected, $result);
+
+        $data = [
+            'description' => 'consult - Rechts 13 min',
+            'gap_id_organization' => 1,
+        ];
+
+        $result = $transformer->transformRowBeforeSave($model, $data);
+
+        $expected = $data;
+        $expected['gap_id_activity'] = 32;
+        $expected['gap_info'] = [
+            'reason' => '13 min',
+            'side' => 'Rechts',
+        ];
+        $this->assertEquals($expected, $result);
+
+        $data = [
+            'description' => 'consult - BDZ 14 min',
+            'gap_id_organization' => 1,
+        ];
+
+        $result = $transformer->transformRowBeforeSave($model, $data);
+
+        $expected = $data;
+        $expected['gap_id_activity'] = 32;
+        $expected['gap_info'] = [
+            'reason' => '14 min',
+            'side' => 'BDZ',
+        ];
+        $this->assertEquals($expected, $result);
+
+        $data = [
+            'description' => 'consult - 15 min',
+            'gap_id_organization' => 1,
+        ];
+
+        $result = $transformer->transformRowBeforeSave($model, $data);
+
+        $expected = $data;
+        $expected['gap_id_activity'] = 32;
+        $expected['gap_info'] = [
+            'reason' => '15 min',
+        ];
+        $this->assertEquals($expected, $result);
+    }
+
     protected function getTransformer()
     {
         $agendaActivityRepositoryProphecy = $this->prophesize(AgendaActivityRepository::class);
