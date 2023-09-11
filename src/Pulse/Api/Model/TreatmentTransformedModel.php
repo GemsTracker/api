da95a19d4d7f5fe2bdded1d76e5668d11b224d3c
Jasper van Gestel <jvangestel@gmail.com>
2023-09-11 12:06:15 +0200

    Cast hand therapy info to int

--- src/Pulse/Api/Model/TreatmentTransformedModel.php
+++ src/Pulse/Api/Model/TreatmentTransformedModel.php
@@ -2,6 +2,8 @@
 
 namespace Pulse\Api\Model;
 
+use Gems\Rest\Fhir\Model\Transformer\IntTransformer;
+
 class TreatmentTransformedModel extends \Ichom\Model\TreatmentTransformedModel
 {
     public function applyBrowseSettings()
@@ -12,5 +14,7 @@ class TreatmentTransformedModel extends \Ichom\Model\TreatmentTransformedModel
             'label' => 'Recovery code',
             'apiName' => 'recoveryCode',
         ]);
+
+        $this->addTransformer(new IntTransformer(['gtrt_hand_therapy_info']));
     }
 }
